5b5be8bbff3b050f3685ef3485a4a53af2347311
Chris Boursnell <cmb211@cam.ac.uk>
2014-06-02 09:53:27 +0000

    added install script

---
+++ install.sh
@@ -0,0 +1,6 @@
+cp bashrc ~/.bashrc
+cp conkyrc ~/.conkyrc
+cp autostart ~/.config/openbox/autostart
+cp menu.xml ~/.config/openbox/menu.xml
+cp rc.xml ~/.config/openbox/rc.xml
+cp terminator-config ~/.config/terminator/config
